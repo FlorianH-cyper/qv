32270cbd5e96a4939fc405be4c520d9ce90c105d
Florian Heitlinger <f.heitlinger@gmx.de>
2021-07-14 13:11:17 +0200

    whatever

--- compare/__init__.py
+++ compare/__init__.py
@@ -22,13 +22,41 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     language = models.StringField(label="please choose your preferred language", choices=["english", "deutsch"], widget=widgets.RadioSelectHorizontal)
-    QV_RULES = models.StringField(label="", choices=["Strongly Agree", "Somewhat Agree", "Neutral", "Somewhat Disagree", "Strongly Disagree"], widget=widgets.RadioSelectHorizontal)
-    MV_RULES = models.StringField(label="", choices=["Strongly Agree", "Somewhat Agree", "Neutral", "Somewhat Disagree", "Strongly Disagree"], widget=widgets.RadioSelectHorizontal)
-
+    QV_RULES_ENG = models.StringField(label="", choices=["Strongly Agree", "Somewhat Agree", "Neutral", "Somewhat Disagree", "Strongly Disagree"], widget=widgets.RadioSelectHorizontal)
+    MV_RULES_ENG = models.StringField(label="", choices=["Strongly Agree", "Somewhat Agree", "Neutral", "Somewhat Disagree", "Strongly Disagree"], widget=widgets.RadioSelectHorizontal)
+    QV_RULES_GER = models.StringField(label="", choices=["Trifft zu", "Trifft eher zu", "teils-teils", "Trifft eher nicht zu", "Trifft eher nicht zu"], widget=widgets.RadioSelectHorizontal)
+    MV_RULES_GER = models.StringField(label="", choices=["Trifft zu", "Trifft eher zu", "teils-teils", "Trifft eher nicht zu", "Trifft eher nicht zu"], widget=widgets.RadioSelectHorizontal)
+    Comparison_GER = models.StringField(label="", choices=["Ich bevorzuge X stark", "Ich bevorzuge X leicht", "Beide gleich gut/schlecht", "Ich bevorzuge Y leicht", "Ich bevorzuge Y stark"], widget=widgets.RadioSelectHorizontal)
+    Comparison_ENG = models.StringField(label="", choices=["I strongly prefer X", "I slightly prefer X",
+                                                           "No difference", "I slightly prefer Y", "I strongly prefer Y"],widget=widgets.RadioSelectHorizontal)
 # PAGES
-class VOTE(Page):
+
+
+class VOTE_GERMAN(Page):
     form_model = "player"
-    form_fields = ["QV_RULES", "MV_RULES"]
+    form_fields = ["QV_RULES_GER", "MV_RULES_GER", "Comparison_GER"]
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.language == "deutsch"
+
+
+class VOTE_ENGLISH(Page):
+    form_model = "player"
+    form_fields = ["QV_RULES_ENG", "MV_RULES_ENG", "Comparison_ENG"]
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.language == "english"
+
+
+class THANKS_GERMAN(Page):
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.language == "deutsch"
+
+class THANKS_ENGLISH(Page):
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.language == "english"
 
 
 class ResultsWaitPage(WaitPage):
@@ -43,4 +71,4 @@ class Results(Page):
     pass
 
 
-page_sequence = [lang, VOTE]
+page_sequence = [lang, VOTE_ENGLISH, VOTE_GERMAN]
